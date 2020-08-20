4149b72eccb9edc457c6ee2fbb02471534cff9b5
thiago soares <thiagopina.soares@gmail.com>
2020-08-19 22:17:06 -0300

    add specialties component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PresentationComponent } from './presentation/presentation.component';
+import { SpecialtiesComponent } from './specialties/specialties.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PresentationComponent
+    PresentationComponent,
+    SpecialtiesComponent
   ],
   imports: [
     BrowserModule,
