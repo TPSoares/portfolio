8595c9b928f647a261d7a95690d25f9eb612fa1e
thiago soares <thiagopina.soares@gmail.com>
2020-08-23 16:08:00 -0300

    add specialties and contact layout to its components

--- src/app/specialties/specialties.component.ts
+++ src/app/specialties/specialties.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecialtiesComponent implements OnInit {
 
+  public specialties: Array<object> = [
+    {
+      title: 'Experiência personalizada',
+      description: 'Obtenha o layout desejado para sua aplicação, dando uma melhor experiência ao seu usuário.',
+      svgPath: '/assets/svg/web-icon.svg',
+
+    },
+    {
+      title: 'Design Responsivo',
+      description: 'Um design responsivo acessível para todos os usuários, indepedente do dispositivo utilizado.',
+      svgPath: '/assets/svg/mobile-friendly.svg',
+
+    },
+    {
+      title: 'Gerenciamento de conteúdo',
+      description: 'Gerencie! Atualize ou adicione conteúdos ao seu negócio sem conhecimentos de código.',
+      svgPath: '/assets/svg/cms.svg',
+
+    },
+  
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
