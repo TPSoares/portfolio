5de3ae9f77ec5eb16946cbcd78ce3c46b9c6ddd7
thiago soares <thiagopina.soares@gmail.com>
2020-08-23 17:38:45 -0300

    add portfolio and footer layout to its components

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+  public projects: Array<object> = [];
+
   constructor() { }
 
   ngOnInit(): void {
