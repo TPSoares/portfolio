1200be6001d15604dbd2f949cf3051c7067814f1
thiago soares <thiagopina.soares@gmail.com>
2020-08-24 22:32:48 -0300

    add scroll on navbar click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,19 @@ import * as AOS from 'aos';
 export class AppComponent implements OnInit {
   title = 'portfolio';
 
+  public component: HTMLElement;
+
   ngOnInit(){
     AOS.init({
       once: true
     });
     AOS.refresh();
+  }
+
+  public ReceiveMessage($event) {
+    let el = document.getElementById($event)
+    const yOffset = -60; 
+    const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({top: y, behavior: 'smooth'});
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  @Output() messageEvent = new EventEmitter<string>();
+
+  public showMenu: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public Scroll(component: string) {
+    this.messageEvent.emit(component)
+    this.showMenu = false;
+  }
+
+  public ShowMenu() {
+    return this.showMenu;
+  }
 }
